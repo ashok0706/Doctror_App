5a55535a7c78765475dbde0be78721754b3d9a85
Ashok.Y <113377234+ashok0706@users.noreply.github.com>
2023-09-07 22:38:30 +0530

    Update DoctorController.java

--- Doctor-APP/DoctorAPP/src/main/java/com/geekster/doctorApp/controller/DoctorController.java
+++ Doctor-APP/DoctorAPP/src/main/java/com/geekster/doctorApp/controller/DoctorController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/doctor")
+@RequestMapping("/doctors")
 public class DoctorController {
 
     @Autowired
